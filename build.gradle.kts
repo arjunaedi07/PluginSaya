0a9753ac8430aadd4ef5ec6c5f56db7f2847add5
Junaa <juna027.my@gmail.com>
2026-02-27 09:39:07 +0700

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,48 @@
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.github.recloudstream:gradle:cce1b8d84d")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.22")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://jitpack.io")
+    }
+}
+
+subprojects {
+
+    apply(plugin = "com.android.library")
+    apply(plugin = "kotlin-android")
+    apply(plugin = "com.lagradost.cloudstream3.gradle")
+
+    cloudstream {
+        setRepo(System.getenv("GITHUB_REPOSITORY")
+            ?: "https://github.com/arjunaedi07/PluginSaya")
+
+        authors = listOf("Arjuna")
+    }
+
+    android {
+        compileSdkVersion(33)
+
+        defaultConfig {
+            minSdk = 21
+            targetSdk = 33
+        }
+    }
+
+    dependencies {
+        "cloudstream"("com.lagradost:cloudstream3:pre-release")
+    }
+}
